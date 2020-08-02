9f4eb15a9df100e5de6d92f287ace196e0387d47
Vazgen Karapetyan <vkarapetyan@irobot.com>
2020-08-02 13:21:23 -0700

    Fixed the destination hostname, should be localhost
    Ok for now, ideally need to use something like rsync.metriffic and make
    the listening service only listen specific interface (this is a security
    issue, see: https://stackoverflow.com/questions/34715797/how-to-use-the-optional-hostname-parameter-in-node-js-server-listen

--- workspace_commands.cpp
+++ workspace_commands.cpp
@@ -1,5 +1,6 @@
 #include "workspace_commands.hpp"
 #include <cxxopts.hpp>
+#include <plog/Log.h>
 #include <regex>
 #include <algorithm>
 #include <sstream>
@@ -52,12 +53,12 @@ workspace_commands::build_rsynch_commandline(std::ostream& out,
         ss << " --delete ";
     }               
     if(direction == SYNC_DIR_DOWN) {
-        ss << username <<"@"<<dest_host<<": "
+        ss << username <<"@localhost: "
             << user_workspace;
     } else 
     if(direction == SYNC_DIR_UP) {
         ss << user_workspace << " "
-            << username << "@" << dest_host << ":";
+            << username << "@localhost:";
         //ss << " /home/vazgen/workspace/METRIFFIC/EXPERIMENTAL/remove_it/b/ "
         //   << sync_username << "@" << dest_host << ":/home/ssh_user/bla/";
     } else {
@@ -78,6 +79,7 @@ workspace_commands::sync(std::ostream& out,
     out<<"requesting access... ";
     int msg_id = m_context.gql_manager.sync_request();
     auto response = m_context.gql_manager.wait_for_response(msg_id);
+    PLOGV << "rsync response: " << std::endl << response.second.dump(4);
     nlohmann::json show_msg = response.second;
 
     if(show_msg["payload"]["data"] != nullptr) {
@@ -100,6 +102,7 @@ workspace_commands::sync(std::ostream& out,
                                                                tunnel_ret.dest_host, tunnel_ret.local_port,
                                                                enable_delete, direction, workspace.second);
                                                                            
+            PLOGV << "rsync commandline: " << commandline;                                                                           
             std::array<char, 32> buffer;
             std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(commandline.c_str(), "r"), pclose);
             if (!pipe) {
@@ -147,7 +150,7 @@ workspace_commands::sync(std::ostream& out,
     } else 
     if(show_msg["payload"]["errors"] != nullptr ) {
         out<<"failed."<<std::endl;
-        std::cout<<"workspace syncrhonization request failed: "<<show_msg["payload"]["errors"][0]["message"]<<std::endl;
+        PLOGE << "workspace syncrhonization request failed: " << show_msg["payload"]["errors"].dump(4);
     }
 }
 
