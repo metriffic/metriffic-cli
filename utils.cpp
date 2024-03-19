128649454ec814f827d9ff42272578a0d1f51c10
vazgenk <vazgen.karapetyan@embodied.me>
2024-03-19 11:10:02 -0700

    Changed the email regexp

--- utils.cpp
+++ utils.cpp
@@ -40,7 +40,7 @@ bool validate_email(const std::string& email)
     // define a regular expression
     const std::regex pattern
         ("(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+");
-        ("^([0-9a-zA-Z]([-.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$");
+        //("^([0-9a-zA-Z]([-.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$");
     // try to match the string with the regular expression
     return std::regex_match(email, pattern);
 }
